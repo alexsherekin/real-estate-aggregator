9e1683d2a25aa6743883cb736db43a3ade797136
Alex Sherekin Home <a.sherekin@snapaddy.com>
2019-05-27 18:27:49 +0200

    Use different http clients for browser and device

--- src/app/shared/third-party-apis/immobilienscout24/connector.service.ts
+++ src/app/shared/third-party-apis/immobilienscout24/connector.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HTTP } from '@ionic-native/http/ngx';
 import { from, Observable, of } from 'rxjs';
@@ -13,6 +14,7 @@ import { UrlCreatorService } from './url.creator.service';
 export class ImmobilienScout24ConnectorService {
   constructor(
     private http: HTTP,
+    private httpClient: HttpClient,
     private urlCreator: UrlCreatorService,
   ) {
 
@@ -20,54 +22,78 @@ export class ImmobilienScout24ConnectorService {
 
   public search(apartment: ApartmentRequirements, search: SearchSettings): Observable<ItemsResponse> {
     const url = this.urlCreator.createSearchUrl(apartment, search);
-    this.http.setDataSerializer('json');
-    return from(this.http.post(url, [], { "Content-Type": "application/json" }))
-      .pipe(
-        delay(2000),
-        map(response => {
-          try {
-            return JSON.parse(response.data)
-          } catch (e) {
-            return undefined;
-          }
-        })
-      );
+    if (window.cordova) {
+      this.http.setDataSerializer('json');
+      return from(this.http.post(url, [], { "Content-Type": "application/json" }))
+        .pipe(
+          delay(2000),
+          map(response => {
+            try {
+              return JSON.parse(response.data)
+            } catch (e) {
+              return undefined;
+            }
+          })
+        );
+    } else {
+      return this.httpClient.post<ItemsResponse>(url, undefined)
+        .pipe(
+          delay(2000)
+        );
+    }
   }
 
   public searchByUrl(url: string): Observable<ItemsResponse> {
     if (!url) {
       return of(undefined);
     }
-    this.http.setDataSerializer('json');
-    return from(this.http.post(this.urlCreator.addBaseUrl(url), [], { "Content-Type": "application/json" }))
-      .pipe(
-        delay(2000),
-        map(response => {
-          try {
-            return JSON.parse(response.data)
-          } catch (e) {
-            return undefined;
-          }
-        })
-      );
+    if (window.cordova) {
+      this.http.setDataSerializer('json');
+      return from(this.http.post(this.urlCreator.addBaseUrl(url), [], { "Content-Type": "application/json" }))
+        .pipe(
+          delay(2000),
+          map(response => {
+            try {
+              return JSON.parse(response.data)
+            } catch (e) {
+              return undefined;
+            }
+          })
+        );
+    } else {
+      return this.httpClient.post<ItemsResponse>(this.urlCreator.addBaseUrl(url), undefined)
+        .pipe(
+          delay(2000)
+        );
+    }
   }
 
   public searchLocation(searchQuery: string): Observable<LocationAutocompleteResponse> {
     const url = this.urlCreator.createSearchLocationUrl(searchQuery);
     this.http.setDataSerializer('json');
-    return from(this.http.get(url, {}, { "Content-Type": "application/json" }))
-      .pipe(
-        delay(2000),
-        map(response => {
-          try {
-            return JSON.parse(response.data)
-          } catch (e) {
-            return undefined;
-          }
-        }),
-        catchError(error => {
-          return of([]);
-        })
-      );
+    if (window.cordova) {
+      return from(this.http.get(url, {}, { "Content-Type": "application/json" }))
+        .pipe(
+          delay(2000),
+          map(response => {
+            try {
+              return JSON.parse(response.data)
+            } catch (e) {
+              return undefined;
+            }
+          }),
+          catchError(error => {
+            return of([]);
+          })
+        );
+    } else {
+      return this.httpClient.get<LocationAutocompleteResponse>(url)
+        .pipe(
+          delay(2000),
+          catchError(error => {
+            return of([]);
+          })
+        );
+    }
   }
 }
