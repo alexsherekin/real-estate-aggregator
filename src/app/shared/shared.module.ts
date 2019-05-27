904a1b64c893086553d9ee9e36ba66a59f9f54ba
Alex Sherekin Home <a.sherekin@snapaddy.com>
2019-05-27 08:22:49 +0200

    Fixes a problem with CORS on real devices

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
 
 import { ImmobilienScout24ConnectorService } from './third-party-apis/immobilienscout24/connector.service';
-import { UrlCreatorService } from './third-party-apis/immobilienscout24/url.creator.service';
 import { ImmobilienScout24DataProvider } from './third-party-apis/immobilienscout24/data/data-provider.service';
 import { LocationAutocompleteService } from './third-party-apis/immobilienscout24/location-autocomplete/location-autocomplete.service';
+import { UrlCreatorService } from './third-party-apis/immobilienscout24/url.creator.service';
 
 @NgModule({
-  imports: [],
   providers: [
     ImmobilienScout24ConnectorService,
     ImmobilienScout24DataProvider,
     LocationAutocompleteService,
     UrlCreatorService,
+    HTTP
   ]
 })
 export class SharedModule { }
--- src/app/shared/third-party-apis/immobilienscout24/connector.service.ts
+++ src/app/shared/third-party-apis/immobilienscout24/connector.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { delay, catchError } from 'rxjs/operators';
+import { HTTP } from '@ionic-native/http/ngx';
+import { from, Observable, of } from 'rxjs';
+import { catchError, delay, map } from 'rxjs/operators';
 
 import { ApartmentRequirements } from '../../types/search-description';
 import { SearchSettings } from '../../types/search-settings';
@@ -12,35 +12,59 @@ import { UrlCreatorService } from './url.creator.service';
 @Injectable({ providedIn: 'root' })
 export class ImmobilienScout24ConnectorService {
   constructor(
-    private http: HttpClient,
+    private http: HTTP,
     private urlCreator: UrlCreatorService,
   ) {
 
   }
 
-  public search(apartment: ApartmentRequirements, search: SearchSettings) {
+  public search(apartment: ApartmentRequirements, search: SearchSettings): Observable<ItemsResponse> {
     const url = this.urlCreator.createSearchUrl(apartment, search);
-    return this.http.post<ItemsResponse>(url, undefined)
+    this.http.setDataSerializer('json');
+    return from(this.http.post(url, [], { "Content-Type": "application/json" }))
       .pipe(
-        delay(2000)
+        delay(2000),
+        map(response => {
+          try {
+            return JSON.parse(response.data)
+          } catch (e) {
+            return undefined;
+          }
+        })
       );
   }
 
-  public searchByUrl(url: string) {
+  public searchByUrl(url: string): Observable<ItemsResponse> {
     if (!url) {
       return of(undefined);
     }
-    return this.http.post<ItemsResponse>(this.urlCreator.addBaseUrl(url), undefined)
+    this.http.setDataSerializer('json');
+    return from(this.http.post(this.urlCreator.addBaseUrl(url), [], { "Content-Type": "application/json" }))
       .pipe(
-        delay(2000)
+        delay(2000),
+        map(response => {
+          try {
+            return JSON.parse(response.data)
+          } catch (e) {
+            return undefined;
+          }
+        })
       );
   }
 
-  public searchLocation(searchQuery: string) {
+  public searchLocation(searchQuery: string): Observable<LocationAutocompleteResponse> {
     const url = this.urlCreator.createSearchLocationUrl(searchQuery);
-    return this.http.get<LocationAutocompleteResponse>(url)
+    this.http.setDataSerializer('json');
+    return from(this.http.get(url, {}, { "Content-Type": "application/json" }))
       .pipe(
         delay(2000),
+        map(response => {
+          try {
+            return JSON.parse(response.data)
+          } catch (e) {
+            return undefined;
+          }
+        }),
         catchError(error => {
           return of([]);
         })
