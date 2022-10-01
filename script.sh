fdc4163bea552762017d139d8376b39d91417116
Oleksandr Sherekin <oleksandr.sherekin@y42.com>
2022-10-01 22:07:27 +0200

    Better curl

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-curl 'https://service.berlin.de/terminvereinbarung/termin/day/' \
+curl -L 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister=327427&anliegen\[\]=318998&herkunft=1' \
   -H 'authority: service.berlin.de' \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
   -H 'accept-language: en-GB,en;q=0.9,en-US;q=0.8,uk;q=0.7,ru;q=0.6,de;q=0.5' \
-  -H 'cache-control: max-age=0' \
-  -H 'cookie: zmsappointment-session=inProgress; Zmsappointment=pq9equgrl7kvao5nqnfms9emj0; wt_rla=102571513503709%2C36%2C1664641235770' \
+  -H 'cookie: zmsappointment-session=inProgress; Zmsappointment=t4e7qvutdaa5iiq7hfn621coio; wt_rla=102571513503709%2C7%2C1664654697430' \
   -H 'dnt: 1' \
   -H 'sec-ch-ua: "Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"' \
   -H 'sec-ch-ua-mobile: ?0' \
@@ -16,7 +15,8 @@ curl 'https://service.berlin.de/terminvereinbarung/termin/day/' \
   -H 'sec-fetch-user: ?1' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53' \
-  --compressed > result.html
+  --compressed
+ > result.html
 
 if grep -q 'An diesem Tag einen Termin buchen' 'result.html'; then
     echo "::set-output name=SEARCH_RESULT::FOUND"
